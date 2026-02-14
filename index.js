a5fc1b14cf2e1604926da773edc27e43f7251836
sukanta022 <sukantacb20@gmail.com>
2026-02-14 19:53:47 +0600

    add middleware for verifyToken

--- index.js
+++ index.js
@@ -5,9 +5,45 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000
 
+
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./bookcouriar-firebase-adminsdk-fbsvc-39bd445182.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+
 //middleware
 app.use(cors())
 app.use(express.json())
+
+
+const verifyFireBaseToken = async (req, res, next) => {
+  console.log("In the verify middleware", req.headers.authorization)
+  // if(!req.headers.authorization){
+  //   //do not allow to go
+  //   return res.status(401).send({message : 'Unauthorized access'})
+  // }
+  // const token = req.headers.authorization.split(' ')[1];
+  // if(!token){
+  //   return res.status(401).send({message : 'Unauthorized access'})
+  // }
+  // //verify token
+  // try{
+  //   const decoded = await admin.auth().verifyIdToken(token)
+  //   req.token_email = decoded.email
+  //   console.log(decoded)
+  //   next();
+  // }
+  // catch{
+  //   console.log("Invalid token")
+  //   return res.status(401).send({message : 'Unauthorized access'})
+  // } 
+  next()
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2q9t7lj.mongodb.net/?appName=Cluster0`;
@@ -55,6 +91,46 @@ async function run() {
         res.status(500).send({ message: "Server error", error });
       }
     });
+
+
+    app.get('/users/:email', async (req, res) => {
+      try {
+        const userEmail = req.params.email
+        const query = { email: userEmail }
+
+        const result = await userCollection.findOne(query)
+
+        if (!result) {
+          return res.status(404).send({ message: "User not found" });
+        }
+        res.send(result)
+      }
+      catch (error) {
+        res.status(500).send({
+          message: "Failed to fetch user",
+          error: error.message
+        });
+      }
+    })
+
+
+    app.patch('/users/:email',  async (req, res) => {
+      const userEmail = req.body
+      // const query = {email: userEmail}
+      
+      // const updateProfile = req.body
+      // const update = {
+      //   $set: {
+      //     name : updateProfile.name,
+      //     photo : updateProfile.photo
+      //   }
+      // }
+
+      // const result = await userCollection.updateOne(query,update)
+      // res.send(result)
+      console.log(userEmail)
+
+    })
 
 
 
